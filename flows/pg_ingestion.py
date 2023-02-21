ed2e0e8bb01ed856ee57681c69e83a9baec3e5a8
Xavier Boakye <fxboakye@yahoo.com>
2023-02-21 16:45:07 -0500

    Amended script to only run extract and transform functions if data doesn't exists in database table.

--- flows/pg_ingestion.py
+++ flows/pg_ingestion.py
@@ -7,6 +7,7 @@ from sqlalchemy import create_engine, text
 from time import time
 import calendar
 from prefect import flow, task
+from datetime import datetime
 from secret import postgres_credentials
 
 @task()
@@ -16,25 +17,29 @@ def extract(color:str, year:int, month:int) -> pd.DataFrame:
     Performs premilinary data cleaning.
     """
 
-    #settting valid arguments
+    #setting valid arguments
+    current_year=int(datetime.now().strftime("%Y"))
     valid_colors=["green", "yellow"]
-    valid_years=list(range(2019,2023))
+    valid_years=list(range(2019, current_year))
     valid_months=list(range(1,13)) 
 
     if color not in valid_colors:
         raise ValueError("Colour must be yellow or green")
     elif year not in valid_years:
-        raise ValueError("Year must be between 2019 and 2022") 
+        raise ValueError(f"Year must be between 2019 and {current_year}") 
     elif month not in valid_months:
         raise ValueError("Month is out of range")
     else:
         parquet_url=f"https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-{month:02}.parquet"
-        data=pd.read_parquet(parquet_url)
+        try:
+            data=pd.read_parquet(parquet_url)
+        except:
+            print("Problem with URL.")
 
-        #changes all column headers to lowercase
-        data.columns=[i.lower() for i in data.columns]
+    #changes all column headers to lowercase
+    data.columns=[i.lower() for i in data.columns]
 
-        return data
+    return data
 
 
 @task()
@@ -52,11 +57,12 @@ def transform_data(data:pd.DataFrame,year:int, month:int) -> pd.DataFrame:
         #for green datasets
         data.rename(columns={"lpep_pickup_datetime":"pickup_datetime","lpep_dropoff_datetime":"dropoff_datetime"}, inplace=True)
     except:
-        try:
-            #for yellow datasets
-            data.rename(columns={"tpep_pickup_datetime":"pickup_datetime","tpep_dropoff_datetime":"dropoff_datetime"}, inplace=True)
-        except:
-            pass
+        pass
+    try:
+        #for yellow datasets
+        data.rename(columns={"tpep_pickup_datetime":"pickup_datetime","tpep_dropoff_datetime":"dropoff_datetime"}, inplace=True)
+    except:
+        print("Columns don't exist")
 
     print(data.dtypes)
 
@@ -82,12 +88,9 @@ def transform_data(data:pd.DataFrame,year:int, month:int) -> pd.DataFrame:
 
     return data
 
-
 @task()
-def ingest_data(color:str, year:str, month:str, data:pd.DataFrame) -> None:
-    """Connects to Postgres Database.
-    Only adds data to the table when the specified months are not present in the database.
-    """
+def pg_connection():
+    """Sets up connection to Postgres Database."""
     #Postgres Credentials
     user=postgres_credentials.user
     pwd=postgres_credentials.password
@@ -96,16 +99,41 @@ def ingest_data(color:str, year:str, month:str, data:pd.DataFrame) -> None:
 
     #Creates connection
     engine=create_engine(f'postgresql://{user}:{pwd}@{host}:{5432}/{db}')
-    conn=engine.connect()
+    
+    return engine
+
+@flow()
+def ingest_data(color:str, year:int, month:int) -> None:
+    """Creates Connection to Database.
+    Skips extraction and transform of data if data exists in table.
+    Only adds data to the table when the specified months or years are not present in the database.
+    Closes database connection afterwards.
+    """
+    try:
+        engine=pg_connection()
+        conn=engine.connect()
+        print('Connection to postgres successful...')
+    except:
+        print("Issue with postgres connection")
     
     table_name=f"{color}_{year}_tripdata"
-    print('Starting ingestion to postgres')
+    
 
-    try:
-        start_time=time()
-        data.to_sql(name=table_name,con=engine, if_exists='fail',index=False, chunksize=25000)
-        print('Total time of ingestion: {time} seconds'.format(time=time()-start_time))
-    except:
+    #checks for tables in database
+    query=conn.execute(text("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'public'"))
+    table_in_db=[row for row in query]
+    print(table_in_db)
+    #checks if table exists before extracting and transforming data. Minimizes compute power.
+    if (table_name,) not in table_in_db:
+            raw_data=extract(color,year,month)             
+            data=transform_data(raw_data,year,month)
+            start_time=time()
+            print(f"Creating table for dataset.")
+            print(f"Starting ingestion to postgres for {calendar.month_name[month]}, {year}...")
+            data.to_sql(name=table_name,con=engine,index=False, chunksize=25000)
+            print('Total time of ingestion: {time} seconds'.format(time=time()-start_time))
+            print(f"Data ingestion for {calendar.month_name[month]}, {year} successful")
+    else:
         try:
             print(f"{table_name} already exists")
             print("Checking if month exists in table....")
@@ -114,42 +142,39 @@ def ingest_data(color:str, year:str, month:str, data:pd.DataFrame) -> None:
             months_in_db=[row for row in result]
             print(months_in_db)
             print((float(month),))
+            #Only appends data if the specified data month doesn't exist in the table
             if (float(month),) not in months_in_db:
-                print(f"Inserting data for {calendar.month_name[month]}")
+                print(f"Inserting data for {calendar.month_name[month]}, {year}")
+                raw_data=extract(color,year,month)             
+                data=transform_data(raw_data,year,month)
+
+                print(f"Starting ingestion to postgres for {calendar.month_name[month]}, {year}...")
                 start_time=time()
                 data.to_sql(name=table_name,con=engine, if_exists='append',index=False, chunksize=25000)
+                print(f"Data ingestion for {calendar.month_name[month]}, {year} successful")
                 print('Total time of ingestion: {time} seconds'.format(time=time()-start_time))
             else:
-                print(f"Dataset already contains data for {calendar.month_name[month]}")
+                print(f"Dataset already contains data for {calendar.month_name[month]}, {year}")
         except:
-            print("Issues")
+            print(f"Encountered an error")
+
 
     conn.close()
 
     return
 
-@flow(log_prints=True)
-def etl_to_pg(month:int, year:int, color: str):
-    raw_data=extract(color,year,month)
-    data=transform_data(raw_data, year,month)
-    ingest_data(color,year,month,data)
-
-    return len(data)
-
 
 
 @flow(log_prints=True)
 def main_etl_flow(months:list, years:list,colors:list):
-    total=0
     for color in colors:
         for year in years:
             for month in months:
-                row_count=etl_to_pg(month, year, color)
-                total +=row_count
-    print(f'Total rows ingested={total}')
+                ingest_data(color, year, month)
 
 if __name__=='__main__':
-    colors=["green"]
-    months=list(range(1,5))
-    years=[2019]
-    main_etl_flow(months, years,colors)
+    colors=["green","yellow"]
+    months=list(range(1,12))
+    years=[2022]
+    main_etl_flow(months, years, colors)
+
